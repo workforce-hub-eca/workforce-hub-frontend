366f42308cfa9b715a1801b1a29a2e10e9e94689
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-08-21 23:34:30 +0530

    chore: refactor for local development and replace GCP storage with local storage

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
 })
